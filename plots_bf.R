e15a634779ad6c397d680200231e455fd26a2e65
a-larval-schmidt <als431@hawaii.edu>
2024-07-29 15:46:03 -1000

    everything

---
+++ plots_bf.R
@@ -0,0 +1,73 @@
+#plotting
+combo<-read.csv("C:/Users/Andrea.Schmidt/Desktop/for offline/combo_whip_slick_short11.csv")
+#libraries####
+library(tidyverse)
+library(lubridate)
+library(readxl)
+library(suncalc)
+library(measurements)
+library(stringr)
+library(geosphere)
+library(sp) #already in raster
+library(raster)# error
+library(ggplot2)
+library(scales)
+library(rgdal)
+library(marmap)
+library(maps)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(rgeos)
+library(viridis)
+library(ggplot2)
+library(raster)
+library(ggnewscale)
+library(sf)
+#map sampling sites######
+world<-ne_countries(scale="medium", returnclass = "sf")
+#oahu_raster <- raster(file.path("C:/Users/Andrea.Schmidt/Documents/M&B_larval_dist_csvs/all_hi_bathy.tiff"))
+oahu_raster <- raster(file.path("C:/Users/Andrea.Schmidt/Desktop/for offline/billfish_hi_bathy.tiff")) #ETPO_2022(Bedrock) from https://www.ncei.noaa.gov/maps/bathymetry
+oahu_df <- fortify(as.bathy(oahu_raster))
+str(oahu_df)
+oahu_map <- ggplot(data=world) +
+  #coord_sf(crs = oahu_raster@crs,xlim=c(-161.5,-154.5),ylim=c(18.85,22.29)) +
+  geom_raster(data = oahu_df, aes(x = x, y = y, fill = z)) +labs(fill = "Depth (m)")+
+  #scale_fill_gradientn(colours = c(grey(50/100), grey(80/100)),values = rescale(c(min(oahu_df$z, na.rm = TRUE), 0,  0.0001, max(oahu_df$z, na.rm = TRUE))), na.value = "transparent",guide = "none")+
+  scale_fill_gradient(high = "lightskyblue1", low = "cornflowerblue",limits=c(-6000,0))+new_scale_fill()+
+  theme(panel.background=element_blank(),axis.title.x = element_blank(), axis.title.y = element_blank())+
+  theme_bw()+geom_sf()+coord_sf(xlim=c(-153,-161), ylim=c(18.7, 22.2))+
+  theme(legend.title=element_text(size=20),legend.text=element_text(size=17),
+        legend.direction = "vertical", legend.box = "vertical", axis.text = element_text(size=20))+
+  scale_x_continuous(breaks=seq((-161), (-153), 1))+
+  scale_y_continuous(breaks = seq(18.7, 22.2,0.5))
+oahu_map+geom_point(data=combo, aes(y=LAT_DD_start, x=LONG_DD_start, color=Year))+
+  scale_color_viridis_c(option="F")+theme(panel.background=element_blank(),axis.title.x = element_blank(), axis.title.y = element_blank())
+
+#kona zoom
+kona_map <- ggplot(data=world) +
+  geom_raster(data = oahu_df, aes(x = x, y = y, fill = z)) +labs(fill = "Depth (m)")+
+  scale_fill_gradient(high = "lightskyblue1", low = "cornflowerblue",limits=c(-6000,0))+new_scale_fill()+
+  theme(panel.background=element_blank(),axis.title.x = "", axis.title.y = "")+#, panel.grid.major.x=element_line())+
+  theme_bw()+geom_sf()+coord_sf(xlim=c(-154.8,-158), ylim=c(18.8, 20.3)) #prev. 158
+kona_map+geom_point(data=combo, aes(y=LAT_DD_start, x=LONG_DD_start, color=Year))+scale_color_viridis_c(option="F")+
+  theme(panel.background=element_blank(),axis.title.x = element_blank(), axis.title.y = element_blank(),legend.title=element_text(size=20),legend.text=element_text(size=17),
+        legend.direction = "vertical", legend.box = "vertical", axis.text = element_text(size=20))+
+  scale_x_continuous(breaks=seq((-158), (-154.8), 0.5))+
+  scale_y_continuous(breaks = seq(18.8, 20.3,0.2))
+
+
+
+mini<-combo_whip_slick_short%>%
+  filter(lat_start>(19.4))%>%
+  filter(lat_start<(19.7))%>%
+  filter(long_start>(-155.95))%>%
+  filter(long_start<(-155.7))
+
+kona_map+geom_label(data=mini, aes(y=lat_start, x=long_start, label=blah), alpha=0.2)
+
+#species distributions#########
+bf<-read.csv("C:/github/billfish_2024/MASTER_BillfishInventory_WHIP+Slicks_Istiophoridae-Xiphiidae_Counts-Sizes_20241305 - BillfishInventory_WHIP+Slicks_Istiophoridae-Xiphiidae_Counts-Sizes_20240202.csv")
+str(bf)
+#site=cruise-station
+bf2<-bf%>%mutate()
+
